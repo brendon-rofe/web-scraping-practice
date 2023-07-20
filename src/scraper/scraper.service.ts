3ef0dc203b9430ca88d1e0dd4132c4dfa36674ca
Bravo1B9 <brofe93@gmail.com>
2023-07-20 08:39:24 +0000

    Implement image download

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 const cheerio = require('cheerio');
 const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 @Injectable()
 export class ScraperService {
@@ -9,22 +11,21 @@ export class ScraperService {
   private baseUrl: string;
   private readonly imageDir = './images';
 
-  public async scrapeTitle(url: string): Promise<string[]> {
+  public async scrapeTitle(url: string): Promise<{}> {
     try {
       this.baseUrl = url;
       const response = await axios.get(url);
       const html = response.data;
       const $ = cheerio.load(html);
-      let imageUrls = [];
 
       await this.checkImageDir();
 
       $('article.product_pod img').each((_idx, el) => {
         const relativeImgSrc = $(el).attr('src');
         const absoluteImgSrc = new URL(relativeImgSrc, this.baseUrl).href;
-        imageUrls.push(absoluteImgSrc)
+        this.downloadImage(absoluteImgSrc);
       })
-      return imageUrls;
+      return { message: 'Images downloaded' };
     } catch (error) {
       console.error(error);
       throw error;
@@ -36,7 +37,18 @@ export class ScraperService {
       await fs.promises.access(this.imageDir);
     } catch (error) {
       await fs.promises.mkdir(this.imageDir);
-    }
+    };
+  };
+
+  private downloadImage(url: string) {
+    const filename = url.split('/').pop();
+    const file = fs.createWriteStream(`${this.imageDir}/${filename}`);
+    const protocol = url.startsWith('https') ? https : http;
+
+    protocol.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', () => file.close());
+    });
   }
 
 };
