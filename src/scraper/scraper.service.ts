578914c62fda1dd3b4b29f031b34bf37156dbfd6
Bravo1B9 <brofe93@gmail.com>
2023-07-20 05:51:22 +0000

    Get image urls

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -5,14 +5,22 @@ const cheerio = require('cheerio');
 @Injectable()
 export class ScraperService {
 
-  public async scrapeTitle(url: string): Promise<string> {
+  private baseUrl: string;
+
+  public async scrapeTitle(url: string): Promise<string[]> {
     try {
+      this.baseUrl = url;
       const response = await axios.get(url);
       const html = response.data;
       const $ = cheerio.load(html);
-      const title = $('head title').text();
+      let imageUrls = [];
 
-      return title;
+      $('article.product_pod img').each((_idx, el) => {
+        const relativeImgSrc = $(el).attr('src');
+        const absoluteImgSrc = new URL(relativeImgSrc, this.baseUrl).href;
+        imageUrls.push(absoluteImgSrc)
+      })
+      return imageUrls;
     } catch (error) {
       console.error(error);
       throw error;
