39b316ce7e0e2a2d51a50336890d48d78bbf6d8a
Bravo1B9 <brofe93@gmail.com>
2023-07-21 07:43:01 +0000

    Download images to AWS S3

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -4,18 +4,24 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
+const AWS = require('aws-sdk');
 
 @Injectable()
 export class ScraperService {
   private readonly imageDir = './images';
   private readonly numPages = 50;
 
-  public async scrapeBookImages(): Promise<{}> {
-    await this.checkImageDir();
-   for(let i = 1; i < this.numPages; i++) {
-    let baseUrl = `http://books.toscrape.com/catalogue/category/books_1/page-${i}.html`
+  private s3 = new AWS.S3({
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: 'us-west-1'
+  })
 
-    try {
+  public async scrapeBookImages(): Promise<{}> {
+    const downloadPromises = [];
+
+    for(let i = 1; i < this.numPages; i++) {
+      let baseUrl = `http://books.toscrape.com/catalogue/category/books_1/page-${i}.html`
       const response = await axios.get(baseUrl);
       const html = response.data;
       const $ = cheerio.load(html);
@@ -23,15 +29,15 @@ export class ScraperService {
       $('article.product_pod img').each((_idx, el) => {
         const relativeImgSrc = $(el).attr('src');
         const absoluteImgSrc = new URL(relativeImgSrc, baseUrl).href;
-        this.downloadImage(absoluteImgSrc);
+        downloadPromises.push(this.downloadAndUploadImage(absoluteImgSrc));
       });
-    } catch (error) {
-      console.error(error);
-      throw error;
     };
-   };
-   return { message: 'Images downloaded' };
+
+    // Wait for all downloads and uploads to finish
+    await Promise.all(downloadPromises);
+    return { message: 'Images downloaded' };
   };
+
 
   private async checkImageDir() {
     try {
@@ -41,15 +47,53 @@ export class ScraperService {
     };
   };
 
-  private downloadImage(url: string) {
-    const filename = url.split('/').pop();
-    const file = fs.createWriteStream(`${this.imageDir}/${filename}`);
-    const protocol = url.startsWith('https') ? https : http;
+  private downloadAndUploadImage(url: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const filename = url.split('/').pop();
 
-    protocol.get(url, (response) => {
-      response.pipe(file);
-      file.on('finish', () => file.close());
+      // Use http or https based on url
+      const protocol = url.startsWith('https') ? https : http;
+      const request = protocol.get(url, (response) => {
+        const data = [];
+        
+        response.on('data', chunk => {
+          data.push(chunk);
+        });
+
+        response.on('end', async () => {
+          const buffer = Buffer.concat(data);
+          
+          // Upload to S3
+          try {
+            await this.uploadToS3(filename, buffer);
+            resolve();
+          } catch (err) {
+            reject(err);
+          }
+        });
+      });
+
+      request.on('error', (err) => {
+        console.log('Error downloading image: ', err);
+        reject(err);
+      });
     });
   };
+
+
+  private async uploadToS3(filename: string, data: Buffer) {
+    const params = {
+      Bucket: 'image-scraping-test', // replace with your bucket name
+      Key: filename, // File name you want to save as in S3
+      Body: data
+    };
+
+    try {
+      const response = await this.s3.upload(params).promise();
+      console.log(`File uploaded successfully at ${response.Location}`);
+    } catch (err) {
+      console.error('Error uploading file: ', err);
+    }
+  }
 
 };
