7b250fb0c0b8d83d3f2034210a27e741d42937f0
Bravo1B9 <brofe93@gmail.com>
2023-07-25 14:15:20 +0000

    Update

--- src/image-scraper/image-scraper.service.ts
+++ src/image-scraper/image-scraper.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import * as fs from 'fs';
 const data = require('/workspace/web-scraping-practice/src/data.json');
 
 @Injectable()
 export class ImageScraperService {
 
-  async returnData() {
-    console.log(data.elements.length);
-    return data.elements;
-  };
+  async downloadImages(): Promise<{}> {
+    fs.mkdirSync('./images', { recursive: true }); // Ensure the directory exists
 
+    for (let i = 0; i < data.elements.length; i++) {
+      const photoId = data.elements[i].photo.split('.')[0]; // Splitting on '.' to get the ID part only
+      const imageUrl = `https://resources.premierleague.com/premierleague/photos/players/250x250/p${photoId}.png`;
+
+      try {
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        fs.writeFileSync(`./images/${photoId}.png`, response.data);
+      } catch (error) {
+        console.log(`Failed to download image for ID ${photoId}: ${error.message}`);
+      }
+    }
+
+    return { message: 'Images download' };
+  }
 };
