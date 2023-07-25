73c114cb1940efcd40734c5e93a589b51a84ef06
Bravo1B9 <brofe93@gmail.com>
2023-07-25 14:41:03 +0000

    Initial AWS S3 setup

--- src/image-scraper/image-scraper.service.ts
+++ src/image-scraper/image-scraper.service.ts
@@ -1,26 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import * as fs from 'fs';
+import * as AWS from 'aws-sdk';
 const data = require('/workspace/web-scraping-practice/src/data.json');
 
 @Injectable()
 export class ImageScraperService {
+  private s3;
+
+  constructor() {
+    this.s3 = new AWS.S3({
+      accessKeyId: '',
+      secretAccessKey: '',
+      region: 'us-east-1'
+    });
+  }
+
+  async uploadFileToS3(file: Buffer, key: string): Promise<any> {
+    const uploadParams = {
+      Bucket: 'your-bucket-name', 
+      Key: key,
+      Body: file
+    };
+
+    return new Promise((resolve, reject) => {
+      this.s3.upload(uploadParams, (err, data) => {
+        if (err) {
+          reject(err);
+        } if (data) {
+          resolve(data);
+        }
+      });
+    });
+  }
 
   async downloadImages(): Promise<{}> {
-    fs.mkdirSync('./images', { recursive: true }); // Ensure the directory exists
-
     for (let i = 0; i < data.elements.length; i++) {
       const photoId = data.elements[i].photo.split('.')[0]; // Splitting on '.' to get the ID part only
       const imageUrl = `https://resources.premierleague.com/premierleague/photos/players/250x250/p${photoId}.png`;
 
       try {
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-        fs.writeFileSync(`./images/${photoId}.png`, response.data);
+        await this.uploadFileToS3(response.data, `${photoId}.png`);
       } catch (error) {
-        console.log(`Failed to download image for ID ${photoId}: ${error.message}`);
-      };
-    };
+        console.log(`Failed to download or upload image for ID ${photoId}: ${error.message}`);
+      }
+    }
 
     return { message: 'Images download' };
-  };
+  }
 };
